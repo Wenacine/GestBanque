347236bcc93d6ee34fcd9ac7334c571bfee4f262
Emi <e.vienne@hotmail.com>
2024-03-22 14:10:10 +0100

    Ajout de la class banque et de l'indexeur

---
+++ Models/Banque.cs
@@ -0,0 +1,53 @@
+﻿namespace Models
+{
+    public class Banque
+    {
+        
+        private string nomBanque { get; set; }
+
+        public string NomBanque
+        {
+            get
+            {
+                return nomBanque;
+            }
+
+            set
+            {
+                nomBanque = value;
+            }
+        }
+
+        private Dictionary<string, Courant> numeroCompte = new Dictionary<string, Courant>(); //Initialiser!
+        public Courant? this[string Key]
+        {
+            get
+            {
+                if(Key == null)
+                {
+                    return null;
+                }
+                if(!numeroCompte.ContainsKey(Key))
+                {
+                    return null;
+                }
+
+                return numeroCompte[Key];
+
+            }
+        }
+
+        public void Ajouter(Courant compte)
+        {
+            numeroCompte.Add(compte.Numero,compte);
+        } 
+
+        public void Supprimer(string numero)
+        {
+            if (!numeroCompte.ContainsKey(numero))
+                return;
+
+                numeroCompte.Remove(numero);
+        }
+    }
+}
